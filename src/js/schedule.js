9cd96fa2a189154e144fb904be414153f44c9443
jacobawenger <jacob@firebase.com>
2014-06-07 11:51:18 -0700

    Started adding more data to the schedule

--- src/js/schedule.js
+++ src/js/schedule.js
@@ -6,6 +6,42 @@ app.controller("scheduleController", ["$scope", "$firebase",
 
     $scope.scheduleYear = "2015";
 
-    $firebase($scope.rootRef.child($scope.scheduleYear)).$bind($scope, "games");
+    $scope.wins = 0;
+    $scope.losses = 0;
+
+    $firebase($scope.rootRef.child($scope.scheduleYear)).$bind($scope, "games").then(function() {
+      console.log($scope.games);
+      for (var key in $scope.games) {
+        if (key[0] !== "$") {
+          var game = $scope.games[key];
+          if (game.notreDameScore > game.opponentScore) {
+            $scope.wins++;
+          }
+          else {
+            $scope.losses++;
+          }
+        }
+      }
+    });
+
+    /* Returns the name of the home team for the inputted game */
+    $scope.getHomeTeamName = function(game) {
+      if (game.isHomeGame) {
+        return "Notre Dame Fighting Irish";
+      }
+      else {
+        return game.opponent.school + game.opponent.mascot;
+      }
+    }
+
+    /* Returns the name of the away team for the inputted game */
+    $scope.getAwayTeamName = function(game) {
+      if (game.isHomeGame) {
+        return game.opponent.school + game.opponent.mascot;
+      }
+      else {
+        return "Notre Dame Fighting Irish";
+      }
+    }
   }
 ]);
