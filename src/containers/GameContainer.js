f2572c72da8c1c2198699fadfeeeee97b37beb9d
Jacob Wenger <wenger.jacob@gmail.com>
2018-09-28 12:11:49 +0100

    Fixed incorrect prop name in GameContainer

--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -4,7 +4,7 @@ import Game from '../components/Game';
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    selected: ownProps.index === state.selectedGameIndex,
+    isSelected: ownProps.index === state.selectedGameIndex,
   };
 };
 
