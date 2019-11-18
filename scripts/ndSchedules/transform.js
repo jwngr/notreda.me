b557c218b1291c264ea801c328f7dfb978342708
jwngr <wenger.jacob@gmail.com>
2019-11-18 12:35:18 -0800

    Reset transform.js script

--- scripts/ndSchedules/transform.js
+++ scripts/ndSchedules/transform.js
@@ -3,22 +3,10 @@ const ndSchedules = require('../lib/ndSchedules');
 
 logger.info('Transforming schedule data...');
 
-const coordinates = {};
 ndSchedules.transformForAllSeasons((gameData, season, gameIndex) => {
   // if (season === 2018) {
   //   delete gameData.result;
   // }
-  const {location} = gameData;
-  if (location !== 'TBD') {
-    const key = `${location.city}-${location.state || location.country}-${location.stadium}`;
-    if (!location.coordinates) {
-      const c = coordinates[key];
-      console.log(season, gameData.opponentId, c, key);
-      gameData.location.coordinates = c;
-    } else {
-      coordinates[key] = location.coordinates;
-    }
-  }
 });
 
 logger.success('Schedule data transformed!');
