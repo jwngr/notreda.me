a8a2d5228e10b72bfba21771f4fc8a450105ffb2
jwngr <wenger.jacob@gmail.com>
2019-12-08 17:15:54 -0800

    Improved team record validation

--- scripts/ndSchedules/validators/validateRecords.js
+++ scripts/ndSchedules/validators/validateRecords.js
@@ -6,13 +6,28 @@ const {CURRENT_SEASON} = require('../../lib/constants');
 const RECORD_REGEX = /^\d{1,2}-\d{1,2}$/;
 const RECORD_REGEX_WITH_TIES = /^\d{1,2}-\d{1,2}-\d{1,2}$/;
 
+const _parseRecord = (record) => {
+  const recordTokens = record.split('-');
+
+  const winCount = Number(recordTokens[0]);
+  const lossCount = Number(recordTokens[1]);
+  const tieCount = recordTokens.length === 2 ? 0 : Number(recordTokens[2]);
+
+  return [winCount, lossCount, tieCount];
+};
+
 module.exports = (
-  {records, season, isHomeGame, weekIndex, completedGameCountForSeason},
+  {records, season, isHomeGame, weekIndex, opponentId, completedGameCountForSeason},
   assert,
   ignoredAssert
 ) => {
   const wrappedAssert = (statement, message) => {
-    assert(statement, message, {records, isHomeGame, weekIndex, completedGameCountForSeason});
+    assert(statement, message, {
+      records,
+      isHomeGame,
+      weekIndex,
+      completedGameCountForSeason,
+    });
   };
 
   const wrappedIgnoredAssert = (statement, message) => {
@@ -52,18 +67,14 @@ module.exports = (
               `${_.capitalize(homeOrAway)} record has unexpected keys.`
             );
 
+            // Validate home, away, and overall records.
             _.forEach(homeOrAwayRecords, (currentRecord, homeAwayOrOverall) => {
               wrappedAssert(
                 currentRecord.match(RECORD_REGEX) || currentRecord.match(RECORD_REGEX_WITH_TIES),
                 `${_.capitalize(homeOrAway)} ${homeAwayOrOverall} record has invalid format.`
               );
 
-              const currentRecordTokens = currentRecord.split('-');
-
-              const winCount = Number(currentRecordTokens[0]);
-              const lossCount = Number(currentRecordTokens[1]);
-              const tieCount =
-                currentRecordTokens.length === 2 ? 0 : Number(currentRecordTokens[2]);
+              const [winCount, lossCount, tieCount] = _parseRecord(currentRecord);
 
               wrappedAssert(
                 isNumber(winCount) && winCount >= 0,
@@ -82,13 +93,53 @@ module.exports = (
                 `${_.capitalize(homeOrAway)} ${homeAwayOrOverall} record has unexpected ties value.`
               );
 
-              if (homeAwayOrOverall === 'overall' && isNdRecord) {
+              if (homeAwayOrOverall === 'overall') {
+                // The number of games Notre Dame has played should be the minimum of the week
+                // index (which is 0-indexed; for weeks with completed games) or the completed
+                // game count (for upcoming games).
+                const ndExpectedGamesPlayed = Math.min(weekIndex + 1, completedGameCountForSeason);
+
+                const gamesPlayedCountFromRecord = winCount + lossCount + tieCount;
+
+                if (isNdRecord) {
+                  // ND's actual games played count from their record should equal their expected
+                  // games played count.
+                  wrappedAssert(
+                    gamesPlayedCountFromRecord === ndExpectedGamesPlayed,
+                    `${_.capitalize(
+                      homeOrAway
+                    )} (ND) overall record has unexpected number of games.`
+                  );
+                } else {
+                  // Opponent's actual games played count from their record should be no more than
+                  // 1 away from the ND's expected games played count.
+                  wrappedAssert(
+                    Math.abs(gamesPlayedCountFromRecord - ndExpectedGamesPlayed) <= 1,
+                    `${_.capitalize(
+                      homeOrAway
+                    )} (opponent) overall record has unexpected number of games.`
+                  );
+                }
+
+                // TODO: Handle neutral site games.
+                const [awayWinCount, awayLossCount, awayTieCount] = _parseRecord(
+                  records[homeOrAway].away
+                );
+                const [homeWinCount, homeLossCount, homeTieCount] = _parseRecord(
+                  records[homeOrAway].home
+                );
+
                 wrappedAssert(
-                  winCount + lossCount + tieCount ===
-                    Math.min(weekIndex + 1, completedGameCountForSeason),
+                  gamesPlayedCountFromRecord ===
+                    awayWinCount +
+                      awayLossCount +
+                      awayTieCount +
+                      homeWinCount +
+                      homeLossCount +
+                      homeTieCount,
                   `${_.capitalize(
                     homeOrAway
-                  )} ${homeAwayOrOverall} record has unexpected number of games.`
+                  )} overall record has different games played count than their away and home records combined.`
                 );
               }
             });
