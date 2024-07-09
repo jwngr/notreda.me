c8e8610237a6c02dea02092f56c6d8c57fd94776
Jacob Wenger <wenger.jacob@gmail.com>
2024-07-09 01:00:43 -0700

    Removed full set of team logos from main bundle (#21)

--- website/src/components/TeamLogo.tsx
+++ website/src/components/TeamLogo.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import defaultLogo from '../images/defaultTeamLogo.png';
 import {Teams} from '../lib/teams';
 import {TeamId} from '../models';
 
-const teamLogos = import.meta.glob('../images/teamLogos/*.png', {eager: true});
+const teamLogos = import.meta.glob('../images/teamLogos/*.png');
 
 export const TeamLogo: React.FC<{
   readonly teamId: TeamId;
   readonly className?: string;
 }> = ({teamId, className}) => {
+  const [logo, setLogo] = useState<string>(defaultLogo);
   const team = Teams.getTeam(teamId);
 
-  const logoModule = teamLogos[`../images/teamLogos/${teamId}.png`] as {default: string};
-  const logo = logoModule ? logoModule.default : defaultLogo;
+  useEffect(() => {
+    const loadLogo = async () => {
+      try {
+        const logoModule = await teamLogos[`../images/teamLogos/${teamId}.png`]();
+        setLogo((logoModule as {default: string}).default);
+      } catch {
+        setLogo(defaultLogo);
+      }
+    };
+
+    loadLogo();
+  }, [teamId]);
 
   return <img key={teamId} className={className} src={logo} alt={`${team.name} logo`} />;
 };
