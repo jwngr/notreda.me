9f1b8a6836280fc93c8d524a16ac74807cedbc37
Jacob Wenger <wenger.jacob@gmail.com>
2024-07-08 18:36:21 -0700

    Added return type

--- website/src/lib/teams.ts
+++ website/src/lib/teams.ts
@@ -4,7 +4,7 @@ import teamsJson from '../resources/teams.json';
 const ALL_TEAMS = teamsJson as Record<TeamId, Team>;
 
 export class Teams {
-  static getTeam(teamId: TeamId) {
+  static getTeam(teamId: TeamId): Team {
     return ALL_TEAMS[teamId];
   }
 }
