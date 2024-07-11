743ecd89e742d51d634973997571ff1d55089ff8
Jacob Wenger <wenger.jacob@gmail.com>
2024-07-11 01:08:04 -0700

    Handled $pageleave and $pageview

--- website/src/lib/posthog.ts
+++ website/src/lib/posthog.ts
@@ -1,6 +1,6 @@
 import posthog from 'posthog-js';
 
-import {POSTHOG_API_HOST, POSTHOG_PROJECT_ID} from './constants';
+import {POSTHOG_API_HOST, POSTHOG_API_KEY} from './constants';
 
 let _areAnalyticsInitialized = false;
 
@@ -8,8 +8,19 @@ export function initAnalytics() {
   if (_areAnalyticsInitialized) return;
   _areAnalyticsInitialized = true;
 
-  posthog.init(POSTHOG_PROJECT_ID, {
+  // Initalize PostHog.
+  posthog.init(POSTHOG_API_KEY, {
     api_host: POSTHOG_API_HOST,
     person_profiles: 'identified_only',
+  });
+
+  // Register $pageleave events.
+  window.addEventListener('beforeunload', () => {
+    posthog.capture('$pageleave');
+  });
+
+  // Register $pageview events on URL navigations. PostHog handles the initial page view itself.
+  window.addEventListener('popstate', () => {
+    posthog.capture('$pageview');
   });
 }
