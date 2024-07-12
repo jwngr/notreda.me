f833246d36e3b0c1589c5fdf1595b5c8fac6f0a1
Jacob Wenger <wenger.jacob@gmail.com>
2024-07-11 21:48:19 -0700

    Switched from PostHog to Cloudflare analytics

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -7,10 +7,6 @@ import './index.css';
 import '@fontsource/bungee';
 import '@fontsource/inter';
 
-import {initAnalytics} from './lib/posthog';
-
-initAnalytics();
-
 const rootDiv = document.getElementById('root');
 if (!rootDiv) {
   throw new Error('Root element not found');
