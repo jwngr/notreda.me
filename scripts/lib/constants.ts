58c0f0a1d06f00d052594fff7053c82bacc29766
Jacob Wenger <wenger.jacob@gmail.com>
2024-08-31 18:45:59 -0700

    Updated current season to 2024 (#67)

--- scripts/lib/constants.ts
+++ scripts/lib/constants.ts
@@ -1,7 +1,7 @@
 import range from 'lodash/range';
 
 export const LATEST_SEASON = 2037;
-export const CURRENT_SEASON = 2023;
+export const CURRENT_SEASON = 2024;
 
 export const ALL_SEASONS = [1887, 1888, 1889, ...range(1892, LATEST_SEASON + 1)];
 export const ALL_PLAYED_SEASONS = [1887, 1888, 1889, ...range(1892, CURRENT_SEASON + 1)];
