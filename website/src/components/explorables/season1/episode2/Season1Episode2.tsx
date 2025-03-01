6e04aaaa359ba65e52fead9c156415d2c84ca1be
Jacob Wenger <wenger.jacob@gmail.com>
2025-03-01 12:01:29 -0800

    Updated all npm deps except React Router [Mar 2025] (#159)

--- website/src/components/explorables/season1/episode2/Season1Episode2.tsx
+++ website/src/components/explorables/season1/episode2/Season1Episode2.tsx
@@ -1,6 +1,5 @@
 import {darken} from 'polished';
 import React, {useMemo, useState} from 'react';
-import {Helmet} from 'react-helmet';
 import {Link} from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -157,9 +156,7 @@ export const ExplorablesS1E2: React.FC = () => {
 
   return (
     <Wrapper>
-      <Helmet>
-        <title>{`${title} | notreda.me`}</title>
-      </Helmet>
+      <title>{`${title} | notreda.me`}</title>
 
       <Heading>
         <Link to="/explorables">Explorables</Link>
