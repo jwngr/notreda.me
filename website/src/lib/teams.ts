56a62434838527cb0d93c859d7eb3b8e1fef799b
jwngr <wenger.jacob@gmail.com>
2025-11-05 23:32:55 -0800

    revert

--- website/src/lib/teams.ts
+++ website/src/lib/teams.ts
@@ -6,8 +6,6 @@ const ALL_TEAMS = teamsJson as Record<TeamId, Team>;
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export class Teams {
   static getTeam(teamId: TeamId): Team {
-    console.log('teamId:', teamId);
-    console.log('ALL_TEAMS:', ALL_TEAMS);
     return ALL_TEAMS[teamId];
   }
 }
